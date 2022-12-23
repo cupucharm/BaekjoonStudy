07340f3a1d7de41a7a71c85fcc4b434faa5b0f31
sujin <tnwlssl0729@naver.com>
2022-12-23 14:45:08 +0900

    221223 boj2587 대표값2

---
+++ boj/src/boj/Boj2587.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Boj2587 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] arr = new int[5];
+		int sum = 0;
+		
+		for (int i = 0; i <5; i++) {
+			arr[i] = scanner.nextInt();
+			sum += arr[i];
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(sum/5);
+		System.out.println(arr[2]);
+		
+		scanner.close();
+	}
+}
