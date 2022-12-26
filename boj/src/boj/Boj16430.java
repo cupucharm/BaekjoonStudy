aad64f84088ab4ffac690d6d627e958d689f9f70
sujin <tnwlssl0729@naver.com>
2022-12-26 13:54:13 +0900

    221226 Boj16430 BufferedReader

--- boj/src/boj/Boj16430.java
+++ boj/src/boj/Boj16430.java
@@ -1,15 +1,21 @@
 package boj;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Boj16430 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int x = scanner.nextInt();
-		int y = scanner.nextInt();
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
 		
 		System.out.println((y-x) + " " + y);
-		scanner.close();
+		br.close();
 	}
 }
