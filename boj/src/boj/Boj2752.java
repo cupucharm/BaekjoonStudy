1657a87979e5c16d02ab59b753853dfda1fe92ad
sujin <tnwlssl0729@naver.com>
2022-12-26 17:58:40 +0900

    221226 Boj2752

---
+++ boj/src/boj/Boj2752.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Boj2752 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] arr = new int[3];
+		
+		for(int i=0; i<3; i++) {
+			arr[i] = scanner.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		for(int i=0; i<3; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+		scanner.close();
+	}
+
+}
