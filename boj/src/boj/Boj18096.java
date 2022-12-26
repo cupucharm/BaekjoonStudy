6d46c890991d73260667c3b47adb7faf800cae27
sujin <tnwlssl0729@naver.com>
2022-12-26 14:05:54 +0900

    221226 Boj18096

---
+++ boj/src/boj/Boj18096.java
@@ -0,0 +1,11 @@
+package boj;
+
+import java.util.Scanner;
+
+public class Boj18096 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("1");
+		scanner.close();
+	}
+}
