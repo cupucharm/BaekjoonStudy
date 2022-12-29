bdf4256611aff8445599709c3eb1860d05ee157d
sujin <tnwlssl0729@naver.com>
2022-12-29 14:57:47 +0900

    221229 Boj5717

---
+++ boj/src/boj/Boj5717.java
@@ -0,0 +1,30 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj5717 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			if (a == 0 && b == 0) {
+				break;
+			}
+
+			sb.append(a + b).append("\n");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+}
